5a39e203032e23e21bf06a260b1ef70464e80fda
harshit <k.lakhina999@gmail.com>
2026-02-01 22:57:45 +0530

    list+tuples+dictionary+sets

--- phase-1/week3.py
+++ phase-1/week3.py
@@ -152,3 +152,139 @@ b={4,5,6}
 c=a.union(b) #union of sets
 d=a.intersection(b) #intersection of sets
 print(a,b,c,d)
+
+#practice questions
+list_tuple=[
+      ("Harshit","Maths"),
+      ("Hitesh","SST"),
+      ("Rohan","chem"),
+      ("Rajat","IT"),
+      ("Rohit","IT"),
+      ("Hiten","IT")
+]
+
+unique_courses=set()
+for tup in list_tuple : #tup will have tuple of size 2 in every iteration
+  unique_courses.add(tup[1])
+
+print(unique_courses)  
+
+for tup in list_tuple :
+  if tup[1]=="IT":
+    print(tup[0])
+
+new_dict={}
+for name,course in list_tuple :
+   if(new_dict.get(name)==None):
+    new_dict.update({name: set()})
+    new_dict[name].add(course)
+   else : 
+    new_dict[name].add(course) 
+
+print(new_dict)    
+
+
+#assignment questions
+#1) palindrome
+string=input("Enter string")
+n=len(string)
+idx=0
+for i in string :
+    if(i!=string[n-1-idx]):
+      print("No")
+    idx+=1
+    if idx==n-1 : print("Yes")
+
+ #2)
+list1=list(map(int,input("Enter list1").split()))
+list2=list(map(int,input("Enter list2").split()))
+
+merge_list=list1+list2
+merge_list.sort()
+sets=set(merge_list)
+print(merge_list)
+print(sets)
+
+list3=list(map(str,input("Enter strings").split()))
+list4=list(map(str,input("Enter strings").split()))
+merge_list2=list3 + list4
+print(" ".join(merge_list2))
+
+#3)
+tup1=tuple(map(int,input("Enter no").split()))
+tup_even=()
+tup_odd=()
+for i in tup1 :
+  if(i%2==0):
+    tup_even=tup_even+(i,)
+  else: tup_odd=tup_odd+(i,)  
+print(tup_even,tup_odd)  
+
+#4)
+diction={}
+
+def add():
+  key=input("Enter name")
+  value=int(input("Enter Marks"))
+  diction[key]=value
+
+def update():
+  inp=input("Enter student name")
+  if(diction.get(inp)==None):
+    print("Not Found")
+  else :
+    new_marks=int(input('Enter new marks'))
+    diction[inp]=new_marks    
+
+def search():
+  val=input("Enter student name")
+  if(diction[val]==None):
+    print("not found")
+  else :
+    print(f"{val} is found with marks {diction[val]}")     
+
+def display():
+  for key,value in diction.items():
+    print(f"{key} : {value}")    
+
+while True:
+  user=input("Enter choice")
+  if user=="A":
+    add()
+  elif user=="U":
+    update()    
+  elif user=="S":
+    search()
+  elif user=="D":
+    display()   
+  else : break  
+
+
+#Q6
+list_words=list(map(str,input("Enter words").split()))
+word_dict={}
+
+for i in list_words :
+  key=i
+  value=len(i)
+  word_dict[key]=value
+
+print(word_dict)
+
+# Q-9
+repeat_list=list(map(int,input("Enter marks").split()))
+repeated=[]
+
+for i in range(1,len(repeat_list)) :
+  if repeat_list[i] == repeat_list[i-1] :
+    if(repeat_list not in repeated):
+      repeated.append(repeat_list[i])
+
+print(repeated)
+
+#Q-10
+user_str=input("Enter string")
+length=len(user_str)
+unique_string= set(user_str)
+print(unique_string)
+print(len(unique_string))
